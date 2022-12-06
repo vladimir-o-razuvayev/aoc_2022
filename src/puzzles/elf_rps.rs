475df69c81fefc66c20474f16d6ffccaa3711f68
Vladimir Razuvayev <vladimir.o.razuvayev@gmail.com>
2022-12-06 16:55:24 -0500

    day 2 part 2

--- src/puzzles/elf_rps.rs
+++ src/puzzles/elf_rps.rs
@@ -10,16 +10,16 @@ pub fn score() -> usize {
                     let l: char = text.as_bytes()[2] as char;
 
                     match l {
-                        'X' => score += 1,
-                        'Y' => score += 2,
-                        'Z' => score += 3,
-                        _ => println!("Invalid Shape"),
+                        'X' => score += 0, // Lose
+                        'Y' => score += 3, // Tie
+                        'Z' => score += 6, // Win
+                        _ => println!("Invalid Command"),
                     }
 
                     match text.as_str() {
-                        "A X" | "B Y" | "C Z" => score += 3,
-                        "A Y" | "B Z" | "C X" => score += 6,
-                        "A Z" | "B X" | "C Y" => score += 0,
+                        "B X" | "A Y" | "C Z" => score += 1, // Rock
+                        "C X" | "B Y" | "A Z" => score += 2, // Paper
+                        "A X" | "C Y" | "B Z" => score += 3, // Scissors
                         _ => println!("Invalid Input"),
                     }
                 }
