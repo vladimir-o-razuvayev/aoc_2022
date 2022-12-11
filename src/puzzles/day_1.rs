3124e87a94ab03e459c3fb996b0e3121b11544d5
Vladimir Razuvayev <vladimir.o.razuvayev@gmail.com>
2022-12-11 15:41:41 -0500

    clean up day 1

--- src/puzzles/day_1.rs
+++ src/puzzles/day_1.rs
@@ -1,24 +1,19 @@
-use crate::prelude::*;
 use std::str::FromStr;
 
 pub fn highest_calories(count: usize) -> usize {
     let mut attempt = 0;
     let mut elves: Vec<usize> = Vec::new();
+    let lines = include_str!("day_1_input.txt").lines();
 
-    if let Ok(lines) = read_lines("src/puzzles/elf_calories_input.txt") {
-        for line in lines {
-            if let Ok(text) = line {
-                if text.is_empty() {
-                    if attempt > 0 {
-                        elves.push(attempt)
-                    }
-                    attempt = 0;
-                } else {
-                    attempt += usize::from_str(&text).unwrap();
-                }
-            }
+    for line in lines {
+        if line.is_empty() {
+            elves.push(attempt);
+            attempt = 0;
+        } else {
+            attempt += usize::from_str(line).unwrap();
         }
     }
+
     elves.sort_by(|a, b| b.cmp(a));
 
     elves.iter().take(count).sum()
