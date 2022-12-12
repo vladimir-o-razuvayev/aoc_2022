f22a07bc9ae2aed7fed1b613e38f3f8793bf6ff5
Vladimir Razuvayev <vladimir.o.razuvayev@gmail.com>
2022-12-12 14:59:18 -0500

    clean up day 2

--- src/puzzles/day_2.rs
+++ src/puzzles/day_2.rs
@@ -1,31 +1,23 @@
-use crate::prelude::*;
-
 pub fn score() -> usize {
-    let mut score = 0;
+    include_str!("day_2_input.txt")
+        .lines()
+        .filter(|l| !l.is_empty())
+        .fold(0, |mut score, line| {
+            let l: char = line.as_bytes()[2] as char;
 
-    if let Ok(lines) = read_lines("src/puzzles/elf_rps_input.txt") {
-        for line in lines {
-            if let Ok(text) = line {
-                if !text.is_empty() {
-                    let l: char = text.as_bytes()[2] as char;
+            match l {
+                'X' => score += 0, // Lose
+                'Y' => score += 3, // Tie
+                'Z' => score += 6, // Win
+                _ => println!("Invalid Command"),
+            };
 
-                    match l {
-                        'X' => score += 0, // Lose
-                        'Y' => score += 3, // Tie
-                        'Z' => score += 6, // Win
-                        _ => println!("Invalid Command"),
-                    }
-
-                    match text.as_str() {
-                        "B X" | "A Y" | "C Z" => score += 1, // Rock
-                        "C X" | "B Y" | "A Z" => score += 2, // Paper
-                        "A X" | "C Y" | "B Z" => score += 3, // Scissors
-                        _ => println!("Invalid Input"),
-                    }
-                }
-            }
-        }
-    }
-
-    score
+            match line {
+                "B X" | "A Y" | "C Z" => score += 1, // Rock
+                "C X" | "B Y" | "A Z" => score += 2, // Paper
+                "A X" | "C Y" | "B Z" => score += 3, // Scissors
+                _ => println!("Invalid Input"),
+            };
+            score
+        })
 }
