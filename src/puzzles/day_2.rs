90c77be1cd5c802fd1f26fb818dfccdc9577618b
Vladimir Razuvayev <vladimir.o.razuvayev@gmail.com>
2022-12-12 15:23:21 -0500

    refactor day 2 from fold to map.sum

--- src/puzzles/day_2.rs
+++ src/puzzles/day_2.rs
@@ -2,22 +2,18 @@ pub fn score() -> usize {
     include_str!("day_2_input.txt")
         .lines()
         .filter(|l| !l.is_empty())
-        .fold(0, |mut score, line| {
-            let l: char = line.as_bytes()[2] as char;
-
-            match l {
-                'X' => score += 0, // Lose
-                'Y' => score += 3, // Tie
-                'Z' => score += 6, // Win
-                _ => println!("Invalid Command"),
-            };
-
-            match line {
-                "B X" | "A Y" | "C Z" => score += 1, // Rock
-                "C X" | "B Y" | "A Z" => score += 2, // Paper
-                "A X" | "C Y" | "B Z" => score += 3, // Scissors
-                _ => println!("Invalid Input"),
-            };
-            score
+        .map(|line| {
+            (match line.as_bytes()[2] as char {
+                'X' => 0, // Lose
+                'Y' => 3, // Tie
+                'Z' => 6, // Win
+                _ => 0,
+            }) + match line {
+                "B X" | "A Y" | "C Z" => 1, // Rock
+                "C X" | "B Y" | "A Z" => 2, // Paper
+                "A X" | "C Y" | "B Z" => 3, // Scissors
+                _ => 0,
+            }
         })
+        .sum()
 }
