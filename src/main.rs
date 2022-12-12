44fa343bc8da48c40a024997a8ed33086b31c72f
Vladimir Razuvayev <vladimir.o.razuvayev@gmail.com>
2022-12-12 15:05:15 -0500

    rename day 3

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,9 @@ mod puzzles;
 fn main() {
     println!("Highest 3 Calories: {:?}", day_1::highest_calories(3));
     println!("RPS Score: {:?}", day_2::score());
-    println!("Rucksack Sum: {:?}", elf_rucksack::sum());
-    println!("Badge Sum: {:?}", elf_rucksack::badge_sum());
+    println!("Rucksack Sum: {:?}", day_3::sum());
+    println!("Badge Sum: {:?}", day_3::badge_sum());
     println!("Overlap Sum: {:?}", day_4::overlap_sum());
     println!("Overlap any Sum: {:?}", day_4::overlap_any_sum());
     println!("Crates: {}", day_5::print_crates());
-}
-
-mod prelude {
-    use std::fs::File;
-    use std::io::{self, BufRead};
-    use std::path::Path;
-
-    pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-    where
-        P: AsRef<Path>,
-    {
-        let file = File::open(filename)?;
-        Ok(io::BufReader::new(file).lines())
-    }
 }
--- src/puzzles/day_3.rs
+++ src/puzzles/day_3.rs
@@ -1,23 +1,18 @@
-use crate::prelude::*;
 use std::collections::HashSet;
 
 pub fn sum() -> usize {
     let mut sum = 0;
 
-    if let Ok(lines) = read_lines("src/puzzles/elf_rucksack_input.txt") {
-        for line in lines {
-            if let Ok(text) = line {
-                if !text.is_empty() {
-                    let (head, tail) = text.split_at(text.len() / 2);
-                    let head: HashSet<u8> = head.bytes().into_iter().collect();
-                    let tail: HashSet<u8> = tail.bytes().into_iter().collect();
-                    for x in head.intersection(&tail) {
-                        if x.is_ascii_uppercase() {
-                            sum += *x as usize - 38;
-                        } else {
-                            sum += *x as usize - 96;
-                        }
-                    }
+    for line in include_str!("day_3_input.txt").lines() {
+        if !line.is_empty() {
+            let (head, tail) = line.split_at(line.len() / 2);
+            let head: HashSet<u8> = head.bytes().into_iter().collect();
+            let tail: HashSet<u8> = tail.bytes().into_iter().collect();
+            for x in head.intersection(&tail) {
+                if x.is_ascii_uppercase() {
+                    sum += *x as usize - 38;
+                } else {
+                    sum += *x as usize - 96;
                 }
             }
         }
@@ -28,7 +23,7 @@ pub fn sum() -> usize {
 
 pub fn badge_sum() -> usize {
     let mut sum = 0;
-    let lines: Vec<&str> = include_str!("elf_rucksack_input.txt").lines().collect();
+    let lines: Vec<&str> = include_str!("day_3_input.txt").lines().collect();
     let groups = lines.chunks(3);
     for group_members in groups {
         let first: HashSet<u8> = group_members[0].bytes().into_iter().collect();
