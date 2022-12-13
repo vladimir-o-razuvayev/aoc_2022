e2a6f1e45688cb36884543a6b0399bb1f7c3ad25
Vladimir Razuvayev <vladimir.o.razuvayev@gmail.com>
2022-12-13 10:23:18 -0500

    refactor day 3

--- src/puzzles/day_3.rs
+++ src/puzzles/day_3.rs
@@ -3,20 +3,13 @@ use std::collections::HashSet;
 pub fn sum() -> usize {
     include_str!("day_3_input.txt")
         .lines()
-        .filter(|l| !l.is_empty())
-        .fold(0, |mut sum, line| {
+        .map(|line| {
             let (head, tail) = line.split_at(line.len() / 2);
             let head: HashSet<u8> = head.bytes().into_iter().collect();
             let tail: HashSet<u8> = tail.bytes().into_iter().collect();
-            for x in head.intersection(&tail) {
-                if x.is_ascii_uppercase() {
-                    sum += *x as usize - 38;
-                } else {
-                    sum += *x as usize - 96;
-                }
-            }
-            sum
+            head.intersection(&tail).map(|x| score(x)).next().unwrap()
         })
+        .sum()
 }
 
 pub fn badge_sum() -> usize {
@@ -24,20 +17,24 @@ pub fn badge_sum() -> usize {
         .lines()
         .collect::<Vec<&str>>()
         .chunks(3)
-        .fold(0, |mut sum, group_members| {
+        .map(|group_members| {
             let first: HashSet<u8> = group_members[0].bytes().into_iter().collect();
             let second: HashSet<u8> = group_members[1].bytes().into_iter().collect();
             let third: HashSet<u8> = group_members[2].bytes().into_iter().collect();
-            for x in first.intersection(&second) {
-                if third.contains(x) {
-                    if x.is_ascii_uppercase() {
-                        sum += *x as usize - 38;
-                    } else {
-                        sum += *x as usize - 96;
-                    }
-                    break;
-                }
-            }
-            sum
+            first
+                .intersection(&second)
+                .filter(|x| third.contains(x))
+                .map(|x| score(x))
+                .next()
+                .unwrap()
         })
+        .sum()
+}
+
+fn score(x: &u8) -> usize {
+    if x.is_ascii_uppercase() {
+        *x as usize - 38
+    } else {
+        *x as usize - 96
+    }
 }
