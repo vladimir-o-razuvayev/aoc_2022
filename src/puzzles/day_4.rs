5e23c5434986a03018ebef44d6f88b39dfde0f3b
Vladimir Razuvayev <vladimir.o.razuvayev@gmail.com>
2022-12-13 10:32:50 -0500

    refactor day 4

--- src/puzzles/day_4.rs
+++ src/puzzles/day_4.rs
@@ -1,43 +1,53 @@
 use regex::Regex;
 
 pub fn overlap_sum() -> usize {
-    let mut sum = 0;
-    let lines: Vec<&str> = include_str!("day_4_input.txt").lines().collect();
     let re = Regex::new(r"^(\d+)-(\d+),(\d+)-(\d+)$").unwrap();
-    for line in lines {
-        for cap in re.captures_iter(line) {
-            let lx = &cap[1].parse::<usize>().unwrap();
-            let ux = &cap[2].parse::<usize>().unwrap();
-            let ly = &cap[3].parse::<usize>().unwrap();
-            let uy = &cap[4].parse::<usize>().unwrap();
-            if uy >= ux && ly <= lx {
-                sum += 1;
-            } else if uy <= ux && ly >= lx {
-                sum += 1;
-            }
-        }
-    }
-    sum
+
+    include_str!("day_4_input.txt")
+        .lines()
+        .map(|line| {
+            re.captures_iter(line)
+                .map(|cap| {
+                    let lx = &cap[1].parse::<usize>().unwrap();
+                    let ux = &cap[2].parse::<usize>().unwrap();
+                    let ly = &cap[3].parse::<usize>().unwrap();
+                    let uy = &cap[4].parse::<usize>().unwrap();
+                    if (uy >= ux && ly <= lx) || (uy <= ux && ly >= lx) {
+                        1
+                    } else {
+                        0
+                    }
+                })
+                .next()
+                .unwrap()
+        })
+        .sum()
 }
 
 pub fn overlap_any_sum() -> usize {
-    let mut sum = 0;
-    let lines: Vec<&str> = include_str!("day_4_input.txt").lines().collect();
     let re = Regex::new(r"^(\d+)-(\d+),(\d+)-(\d+)$").unwrap();
-    for line in lines {
-        for cap in re.captures_iter(line) {
-            let lx = &cap[1].parse::<usize>().unwrap();
-            let ux = &cap[2].parse::<usize>().unwrap();
-            let ly = &cap[3].parse::<usize>().unwrap();
-            let uy = &cap[4].parse::<usize>().unwrap();
-            if (uy >= ux && ly <= lx)
-                || (uy <= ux && ly >= lx)
-                || (uy >= ux && ly <= ux)
-                || (uy >= lx && ly <= lx)
-            {
-                sum += 1;
-            }
-        }
-    }
-    sum
+
+    include_str!("day_4_input.txt")
+        .lines()
+        .map(|line| {
+            re.captures_iter(line)
+                .map(|cap| {
+                    let lx = &cap[1].parse::<usize>().unwrap();
+                    let ux = &cap[2].parse::<usize>().unwrap();
+                    let ly = &cap[3].parse::<usize>().unwrap();
+                    let uy = &cap[4].parse::<usize>().unwrap();
+                    if (uy >= ux && ly <= lx)
+                        || (uy <= ux && ly >= lx)
+                        || (uy >= ux && ly <= ux)
+                        || (uy >= lx && ly <= lx)
+                    {
+                        1
+                    } else {
+                        0
+                    }
+                })
+                .next()
+                .unwrap()
+        })
+        .sum()
 }
