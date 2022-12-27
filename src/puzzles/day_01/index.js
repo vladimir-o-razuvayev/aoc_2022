d1438d7c90db234706e7b852e79bbc472f5e61b6
Vladimir Razuvayev <vladimir.o.razuvayev@gmail.com>
2022-12-27 16:43:55 -0500

    set up skeleton

--- src/puzzles/day_01/index.js
+++ src/puzzles/day_01/index.js
@@ -1,11 +1,29 @@
 import example from './example.txt';
+import input from './input.txt';
 import { highest_calories } from '../../../pkg';
 
-const p = document.createElement('p');
 const body = document.querySelector('body');
+
+const select = document.createElement('select');
+select.add(new Option('Example', "0"), undefined);
+select.add(new Option('Input', "1"), undefined);
+body.appendChild(select);
+
+const p = document.createElement('p');
 p.innerHTML = example;
 body.appendChild(p);
 
+const number_field = document.createElement('input');
+number_field.type = 'number';
+body.appendChild(number_field);
+
 const p2 = document.createElement('p');
-p2.innerHTML = highest_calories(example, 3);
 body.appendChild(p2);
+
+number_field.onchange = function () {
+    if (select.selectedIndex === 0) {
+        p2.innerHTML = highest_calories(example, number_field.valueAsNumber);
+    } else {
+        p2.innerHTML = highest_calories(input, number_field.valueAsNumber);
+    }
+}
