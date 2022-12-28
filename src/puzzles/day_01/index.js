e83bac7a9cc5131e08c7bb681eeb83ac23e54311
Vladimir Razuvayev <vladimir.o.razuvayev@gmail.com>
2022-12-27 21:29:42 -0500

    trying out bulma styling

--- src/puzzles/day_01/index.js
+++ src/puzzles/day_01/index.js
@@ -2,46 +2,23 @@ import example from './example.txt';
 import input from './input.txt';
 import { highest_calories } from '../../../pkg';
 
-const body = document.querySelector('#content');
-
-const select = document.createElement('select');
-select.add(new Option('Example', "0"), undefined);
-select.add(new Option('Input', "1"), undefined);
-body.appendChild(select);
-
-const div = document.createElement('div')
-div.style = 'width:80px; height:280px; border:solid; overflow:scroll;';
-body.appendChild(div);
-
-const code = document.createElement('code');
-code.style = 'white-space: pre-wrap'
-code.innerHTML = example;
-div.appendChild(code);
-
-const number_field = document.createElement('input');
-number_field.type = 'number';
-number_field.setAttribute('min', '0');
-number_field.defaultValue = '0';
-body.appendChild(number_field);
-
-const p2 = document.createElement('p');
-p2.innerHTML = '0';
-body.appendChild(p2);
+const title = document.querySelector('.title');
+title.innerHTML = "Day 1";
+const select = document.querySelector('#puzzle_input');
+const code = document.querySelector('#display_input');
+code.value = example;
+const number_field = document.querySelector('#puzzle_value');
+const result = document.querySelector('#result');
 
 number_field.onchange = function () {
-    if (select.selectedIndex === 0) {
-        p2.innerHTML = highest_calories(example, number_field.valueAsNumber);
-    } else {
-        p2.innerHTML = highest_calories(input, number_field.valueAsNumber);
-    }
+    result.innerHTML = highest_calories(code.value, number_field.valueAsNumber);
 }
 
 select.onchange = function () {
     if (select.selectedIndex === 0) {
-        p2.innerHTML = highest_calories(example, number_field.valueAsNumber);
-        code.innerHTML = example;
+        code.value = example;
     } else {
-        p2.innerHTML = highest_calories(input, number_field.valueAsNumber);
-        code.innerHTML = input;
+        code.value = input;
     }
+    result.innerHTML = highest_calories(code.value, number_field.valueAsNumber);
 }
